22355f47c715f5edfe6afad4d9417ccb0be686b7
2014301008 <31638928+2014301008@users.noreply.github.com>
2017-09-05 23:31:14 +0900

    Add files via upload

---
+++ assignment_1st.java
@@ -0,0 +1,12 @@
+public class assignment_1st {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Id Number : 2014301008");
+		System.out.println(" Name : ±¸µ¿±Ù ");
+		System.out.println(" Phone Number : 010 - 9689 -5811");
+		System.out.println(" Github Address : github.com/2014301008 ");
+
+	}
+
+}
